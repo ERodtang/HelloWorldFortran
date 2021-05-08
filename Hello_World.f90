42101f88a89c70ff16f15cbb7497687746786360
ERodtang <infamous.badger@protonmail.com>
2021-05-08 15:09:30 +0200

    Added comments

--- Hello_World.f90
+++ Hello_World.f90
@@ -1,3 +1,10 @@
+!Run on:
+!Visual Code Studio
+!Debian 4.19
+!Remember to:
+!Install gdb and gfortran
+!configure launch.json file
+
 program Hello_World
     print *, 'Hello World!'
 end program Hello_World
